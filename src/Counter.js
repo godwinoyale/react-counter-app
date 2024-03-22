a159d4b38b962d1396ec1c4e81e762d60228c8ce
Godwin <godwinoyale@gmail.com>
2024-03-22 13:45:01 +0100

    created full-functional buttons for increment and decrement

--- src/Counter.js
+++ src/Counter.js
@@ -2,14 +2,14 @@ import { useState } from "react"
 
 const Counter = () => {
 
-    const [count, setCount] = useState()
+    const [count, setCount] = useState(0)
 
     const increment = () => setCount(count + 1)
     const decrement = () => setCount(count - 1)
   return (
     <>
     <div className="container">
-        <h1>Count: {count}</h1>
+        <h1>Count : {count}</h1>
         <button onClick={increment} className="plus">+</button>
         <button onClick={decrement} className="minus">-</button>
     </div>
