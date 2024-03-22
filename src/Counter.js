305770bafe928fe9cd306eb359835a4da5d20a59
Godwin <godwinoyale@gmail.com>
2024-03-22 13:34:00 +0100

    added to increase and decrease buttons

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,19 @@
-
+import { useState } from "react"
 
 const Counter = () => {
+
+    const [count, setCount] = useState()
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
   return (
-    <div>Counter</div>
+    <>
+    <div className="container">
+        <h1>Count: {count}</h1>
+        <button onClick={increment} className="plus">+</button>
+        <button onClick={decrement} className="minus">-</button>
+    </div>
+    </>
   )
 }
 
